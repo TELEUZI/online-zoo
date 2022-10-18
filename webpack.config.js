a1e7d9df14c120d87e3f39951c5e558cda19637b
Yauheni Tsurankou <Yauheni_Tsurankou@epam.com>
2022-10-18 18:04:53 +0000

    fix: fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app.ts',
   module: {
-    rules: [{
-      test: [/\.tsx?$/],
-      use: ['ts-loader'],
-      exclude: /node_modules/,
-    }, {
-      test: /.scss$/,
-      exclude: [/node_modules/],
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }, {
-      test: /.(?:ico|gif|png|jpg|svg)$/,
-      type: 'asset/resource',
-    }],
+    rules: [
+      {
+        test: [/\.tsx?$/],
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /.scss$/,
+        exclude: [/node_modules/],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(?:ico|gif|png|jpg|svg)$/,
+        type: 'asset/resource',
+      },
+    ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+  plugins: [new HtmlWebpackPlugin()],
+
   mode: 'development',
   output: {
     filename: 'bundle.js',
