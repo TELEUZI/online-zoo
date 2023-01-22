c485c0ef85315dab84d7b19950e3e50dea9e7e31
Yauheni Tsurankou <Yauheni_Tsurankou@epam.com>
2023-01-22 01:15:48 +0000

    fix: fix routing

--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -1,13 +1,20 @@
 import './link.scss';
 import BaseComponent from '../base-component';
+import Router from '../../router';
 
 export default class Link extends BaseComponent {
-  onClick: () => void = () => {};
-
-  constructor(textContent: string, href: string, onClick?: () => void) {
+  constructor(textContent: string, href: string) {
     super('a', ['link'], textContent);
     this.setAttribute('href', href);
-    this.onClick = onClick;
-    this.node.onclick = () => this.onClick?.();
+    this.node.addEventListener('click', this.changeRoute);
   }
+
+  private changeRoute = (event: Event) => {
+    event.preventDefault();
+    const { currentTarget } = event;
+    if (currentTarget instanceof HTMLAnchorElement) {
+      window.history.pushState({}, '', currentTarget.href);
+      Router.onPathChangeHandler();
+    }
+  };
 }
